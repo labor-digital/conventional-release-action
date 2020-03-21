17df01072c71b8b124571558f3e6c6b0f8aaaf9c
Martin Neundorfer <m.neundorfer@labor.digital>
2020-03-21 17:44:22 +0100

    chore: try to fix the release issues

--- src/ReleaseHandler.js
+++ src/ReleaseHandler.js
@@ -53,7 +53,7 @@ module.exports = class ReleaseAction {
 
 			// Check if there are special bump actions to perform
 			const filename = path.join(rootDirectory, "after-release.js");
-			console.log("node " + filename + " " + newVersion, require("fs").existsSync(filename));
+			core.info("node " + filename + " " + newVersion + " " + require("fs").existsSync(filename));
 			if (require("fs").existsSync(filename))
 				environment.scripts.postbump = "node " + filename + " " + newVersion;
 
